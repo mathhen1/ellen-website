345515bcacb0a79897e23337c15e72ceaf4fc5fe
mathen <math.z.mhj@gmail.com>
2026-06-09 19:58:34 -0300

    fix(style): fix lateral bar layout overflowing

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -21,18 +21,18 @@ const NavBar = () => {
     }
 
     return (
-        <div className="relative w-screen flex">
-            <div className={"w-screen sticky top-0 flex flex-row font-roboto border-b-2 border-fuchsia-600 p-2 bg-fuchsia-400"}>
-                <h1 className="flex-1 text-2xl pl-5 font-bold tracking-wider">Ellen Souza</h1>
+        <div className="relative w-screen flex h-full">
+            <div className={"w-screen flex flex-row font-roboto border-b-2 border-fuchsia-600 p-2 bg-fuchsia-400"}>
+                <h1 className="flex-1 text-2xl pl-5 font-bold tracking-wider">Promotora Ellen</h1>
                 <Menu className="flex-none" onClick={handleMenu} />
             </div>
-            <div className={"absolute z-10 h-screen w-screen flex justify-end backdrop-blur "  + showMenu}>
-                <div className={"h-screen flex flex-col items-center bg-fuchsia-400 w-3/4 border-3 p-3 gap-2 " + animateMenuOpening}>
+            <div className={"overflow-x-hidden absolute z-10 h-screen w-full flex justify-end backdrop-blur " + showMenu}>
+                <div className={"absolute h-full flex flex-col items-center bg-fuchsia-400 w-3/4 border-3 p-3 gap-2 " + animateMenuOpening}>
                     <Menu className="flex-none" onClick={handleMenu} />
-                    <a href="">Sobre Mim</a>
-                    <a href="">Experiencias</a>
-                    <a href="">Formação</a>
-
+                    <a href="#sobre-mim">Sobre Mim</a>
+                    <a href="experiencia">Experiencia</a>
+                    <a href="formacao">Formação</a>
+                    <a href="contato">Contato</a>
                 </div>
             </div>
         </div>
