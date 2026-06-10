6bf7412019d59e74b5b8579edfcab1697d6aae1e
mathen <math.z.mhj@gmail.com>
2026-06-10 01:14:12 -0300

    style: add all cards content of experience w/ snap

--- app/Components/Experience.tsx
+++ app/Components/Experience.tsx
@@ -1,40 +1,96 @@
 const Experience = () => {
 
-    const cardXp = "rounded-lg object-fit w-11/12 h-[60vh] flex flex-col items-center justify-start bg-fuchsia-400 border-2 border-b-4 border-l-4 overflow-auto flex-shrink-0 snap-center gap-2 p-3"
+    const cardXp = "rounded-lg object-fit w-11/12 min-h-[60vh] h-full flex flex-col items-center justify-start bg-fuchsia-400 border-2 border-b-4 border-l-4 overflow-auto flex-shrink-0 snap-center snap-always gap-2 p-3"
     const imageXp = "rounded-4xl object-fit w-10/12 h-60 border-3 border-r-5 border-b-5 border-black"
 
     return (
         <div className="flex flex-col gap-3 items-center justify-center">
-            <h1 className="font-black text-fuchsia-800 tracking-wider text-xl border-b-2">
-                Experiencia
+            <h1 className="reveal opacity-0 font-black text-fuchsia-800 tracking-wider text-xl border-b-2">
+                Experiência
             </h1>
 
-            <div className="flex flex-row gap-3 
+            <div className="reveal opacity-0 flex flex-row gap-3 
                 overflow-x-auto h-full m-1 p-1 
                 w-full snap-x snap-mandatory">
 
                 <div className={cardXp}>
                     <img src="ellen-imperio1.jpg" className={imageXp} />
 
-                    <h1>Oiiiiiiiiiiiiiiiii</h1>
+                    <h1 className="text-lg text-fuchsia-900 font-extrabold tracking-wider">
+                        Cerveja Imperio
+                    </h1>
+
+                    <h2 className="w-10/12 text-center text-sm font-bold">
+                        Ação de desgustação com impulsionamento de Vendas da Cerveja Imperio
+                    </h2>
+                    <p className="text-xs font-semibold w-10/12 text-center">
+                        A Cerveja Império é uma grande marca brasileira de cerveja do tipo puro malte, pertencente à Cervejaria Cidade Imperial. É conhecida pelo seu ótimo custo-benefício e alta drinkability.
+                    </p>
                 </div>
 
                 <div className={cardXp}>
 
                     <img src="ellen-imperio2.jpg" className={imageXp} />
 
+                    <h1 className="text-lg text-fuchsia-900 font-extrabold tracking-wider">
+                        Cerveja Imperio
+                    </h1>
+
+                    <h2 className="w-10/12 text-center text-sm font-bold">
+                        Ação de desgustação com impulsionamento de Vendas da Cerveja Imperio
+                    </h2>
+                    <p className="text-xs font-semibold w-10/12 text-center">
+                        A Cerveja Império é uma grande marca brasileira de cerveja do tipo puro malte, pertencente à Cervejaria Cidade Imperial. É conhecida pelo seu ótimo custo-benefício e alta drinkability.
+                    </p>
+
                 </div>
 
                 <div className={cardXp}>
                     <img src="ellen-bet.jpg" className={imageXp} />
+
+                    <h1 className="text-lg text-fuchsia-900 font-extrabold tracking-wider">
+                        Bet dá Sorte
+                    </h1>
+
+                    <h2 className="w-10/12 text-center text-sm font-bold">
+                        Ação de Promoção e Divulgação da Bet dá Sorte
+                    </h2>
+
+                    <p className="text-xs font-semibold w-10/12 text-center">
+                        A Bet Dá Sorte é uma plataforma brasileira de apostas online de quota fixa que opera no segmento de apostas esportivas. Regulamentada conforme as diretrizes da legislação nacional, a marca ganhou relevância no mercado nacional.
+                    </p>
                 </div>
 
                 <div className={cardXp}>
                     <img src="ellen-matuta1.jpg" className={imageXp} />
+
+                    <h1 className="text-lg text-fuchsia-900 font-extrabold tracking-wider">
+                        Cachaça Matuta
+                    </h1>
+
+                    <h2 className="w-10/12 text-center text-sm font-bold">
+                        Ação de Desgustação com direito a troca de brinde
+                    </h2>
+
+                    <p className="text-xs font-semibold w-10/12 text-center">
+                        A Cachaça Matuta é uma premiada marca artesanal produzida no Engenho Vaca Brava, na Paraíba. Ela se destaca no mercado pelo alto padrão de qualidade e forte identidade nordestina.
+                    </p>
                 </div>
 
                 <div className={cardXp}>
                     <img src="ellen-matuta2.jpg" className={imageXp} />
+
+                    <h1 className="text-lg text-fuchsia-900 font-extrabold tracking-wider">
+                        Cachaça Matuta
+                    </h1>
+
+                    <h2 className="w-10/12 text-center text-sm font-bold">
+                        Ação de Desgustação com direito a troca de brinde
+                    </h2>
+
+                    <p className="text-xs font-semibold w-10/12 text-center">
+                        A Cachaça Matuta é uma premiada marca artesanal produzida no Engenho Vaca Brava, na Paraíba. Ela se destaca no mercado pelo alto padrão de qualidade e forte identidade nordestina.
+                    </p>
                 </div>
 
             </div>
