3088037c71f66603833d63ffd25abef9ab9f8c3f
mathen <math.z.mhj@gmail.com>
2026-06-10 01:25:02 -0300

    feat: add components and intersection observer w/ animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
 "use client"
 
+import { useEffect } from "react";
+import AboutMe from "./Components/AboutMe";
+import AboutMeDetails from "./Components/AboutMeDetails";
 import NavBar from "./Components/NavBar";
+import Experience from "./Components/Experience";
 
 export default function Home() {
+
+  useEffect(() => {
+    const root = document.querySelectorAll(".reveal")
+
+    const obs = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate-reveal")
+        }
+      })
+    })
+
+    root.forEach(node => {
+      obs.observe(node)
+    })
+
+  }, [])
+
   return (
-    <div className="h-full w-screen">
+    <div className="h-full w-screen overflow-clip">
+      <div className="sticky -top-1 z-10 h-full">
         <NavBar />
+      </div>
+
+      <AboutMe />
+
+      <AboutMeDetails />
+
+      <Experience />
     </div>
   );
 }
