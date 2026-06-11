97691758cfa4ab563d389789aa6b8bc696e2c069
mathen <math.z.mhj@gmail.com>
2026-06-10 22:29:49 -0300

    style: add more content and cards

--- app/Components/Experience.tsx
+++ app/Components/Experience.tsx
@@ -1,7 +1,7 @@
 const Experience = () => {
 
-    const cardXp = "rounded-lg object-fit w-11/12 min-h-[60vh] h-full flex flex-col items-center justify-start bg-fuchsia-400 border-2 border-b-4 border-l-4 overflow-auto flex-shrink-0 snap-center snap-always gap-2 p-3"
-    const imageXp = "rounded-4xl object-fit w-10/12 h-60 border-3 border-r-5 border-b-5 border-black"
+    const cardXp = "rounded-lg w-11/12 min-h-[70vh] h-full flex flex-col items-center justify-center bg-fuchsia-400 border-2 border-b-4 border-l-4 overflow-auto flex-shrink-0 snap-center snap-always gap-2 p-3 "
+    const imageXp = "rounded-4xl object-cover w-10/12 h-80 border-3 border-r-5 border-b-5 border-black"
 
     return (
         <div className="flex flex-col gap-3 items-center justify-center">
@@ -10,23 +10,8 @@ const Experience = () => {
             </h1>
 
             <div className="reveal opacity-0 flex flex-row gap-3 
-                overflow-x-auto h-full m-1 p-1 
+                overflow-x-auto h-full m-1 p-1 scrollbar-none 
                 w-full snap-x snap-mandatory">
-
-                <div className={cardXp}>
-                    <img src="ellen-imperio1.jpg" className={imageXp} />
-
-                    <h1 className="text-lg text-fuchsia-900 font-extrabold tracking-wider">
-                        Cerveja Imperio
-                    </h1>
-
-                    <h2 className="w-10/12 text-center text-sm font-bold">
-                        Ação de desgustação com impulsionamento de Vendas da Cerveja Imperio
-                    </h2>
-                    <p className="text-xs font-semibold w-10/12 text-center">
-                        A Cerveja Império é uma grande marca brasileira de cerveja do tipo puro malte, pertencente à Cervejaria Cidade Imperial. É conhecida pelo seu ótimo custo-benefício e alta drinkability.
-                    </p>
-                </div>
 
                 <div className={cardXp}>
 
@@ -43,6 +28,21 @@ const Experience = () => {
                         A Cerveja Império é uma grande marca brasileira de cerveja do tipo puro malte, pertencente à Cervejaria Cidade Imperial. É conhecida pelo seu ótimo custo-benefício e alta drinkability.
                     </p>
 
+                </div>
+
+                <div className={cardXp}>
+                    <img src="ellen-imperio1.jpg" className={imageXp} />
+
+                    <h1 className="text-lg text-fuchsia-900 font-extrabold tracking-wider">
+                        Cerveja Imperio
+                    </h1>
+
+                    <h2 className="w-10/12 text-center text-sm font-bold">
+                        Ação de desgustação com impulsionamento de Vendas da Cerveja Imperio
+                    </h2>
+                    <p className="text-xs font-semibold w-10/12 text-center">
+                        A Cerveja Império é uma grande marca brasileira de cerveja do tipo puro malte, pertencente à Cervejaria Cidade Imperial. É conhecida pelo seu ótimo custo-benefício e alta drinkability.
+                    </p>
                 </div>
 
                 <div className={cardXp}>
@@ -94,6 +94,86 @@ const Experience = () => {
                 </div>
 
             </div>
+
+            <h1 className="reveal opacity-0 text-black tracking-wide w-10/12
+            text-lg text-center font-black tracking-wider
+            bg-clip-text text-transparent bg-gradient-to-r
+            from-fuchsia-800 via-fuchsia-600 to-fuchsia-800
+            border-black border-b-2 border-t-2 p-2 border-fuchsia-500">
+                Retratando seu produto com beleza e delicadeza
+            </h1>
+
+            <div className="reveal opacity-0 w-11/12 bg-fuchsia-400 p-3 rounded-lg 
+                flex flex-col items-center mb-1
+                border-2 border-fuchsia-900 border-b-4">
+
+                <h1 className="reveal opacity-0 text-lg font-black tracking-wider mb-2">
+                    Com quem já trabalhei?
+                </h1>
+
+                <div className="reveal opacity-0 flex flex-row gap-5 w-full justify-center text-fuchsia-900 font-black">
+
+                    <ul className="reveal opacity-0 text-sm tracking-wide text-start list-disc">
+                        <li className="reveal opacity-0">
+                            Cerveja Imperio
+                        </li>
+                        <li className="reveal opacity-0">
+                            Bet dá Sorte
+                        </li>
+                        <li className="reveal opacity-0">
+                            Agência Zazo
+                        </li>
+                        <li className="reveal opacity-0">
+                            Tanqueray
+                        </li>
+                        <li className="reveal opacity-0">
+                            Ketel One
+                        </li>
+                    </ul>
+
+                    <ul className="reveal opacity-0 text-sm tracking-wide text-start list-disc">
+                        <li className="reveal opacity-0">
+                            Sport Club Recife
+                        </li>
+                        <li className="reveal opacity-0">
+                            Grand Bares e Eventos
+                        </li>
+
+                        <li className="reveal opacity-0">
+                            Cerveja Petra
+                        </li>
+
+                        <li className="reveal opacity-0">
+                            Itaipava
+                        </li>
+
+                        <li className="reveal opacity-0">
+                            Ótica Ratts
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <h2 className="reveal opacity-0 w-11/12 text-center bg-fuchsia-400
+            rounded-md font-black p-2 tracking-wider text-nowrap border-b-2 border-l-2 border-fuchsia-800">
+                Estou aberta a novas oportunidades
+            </h2>
+
+            <div className="reveal opacity-0 w-11/12 text-center bg-gradient-to-r 
+            from-fuchsia-500 via-fuchsia-400 to-fuchsia-500 p-2
+            rounded-md">
+                <h2 className="reveal opacity-0 text-black font-black tracking-wide text-white italic">
+                    O que está esperando?
+                </h2>
+            </div>
+
+            <h2 className="reveal opacity-0 w-11/12 text-center font-black
+                p-2 border-black border-2 border-b-4 border-l-4 mb-3">
+                Me contrate
+            </h2>
+
+            {/* <span className="reveal opacity-0 h-px w-10/12 border boder-black mb-3"></span> */}
 
         </div>
     )
