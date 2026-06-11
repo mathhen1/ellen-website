e84adcf5dedbd8fd7a04e18601d8a1328dc2fb2c
mathen <math.z.mhj@gmail.com>
2026-06-10 22:24:18 -0300

    chore: add one more device w/ access

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.0.102', '192.168.0.103']
+  allowedDevOrigins: ['192.168.0.102', '192.168.0.103', '192.168.0.104']
 };
 
 export default nextConfig;
