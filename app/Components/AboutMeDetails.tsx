aae3310880ffe442fdfff2da2bc3d7d354502a4b
mathen <math.z.mhj@gmail.com>
2026-06-12 01:50:01 -0300

    feat(style): add card extra skills and change layout color

--- app/Components/AboutMeDetails.tsx
+++ app/Components/AboutMeDetails.tsx
@@ -24,20 +24,50 @@ const AboutMeDetails = () => {
                     bg-fuchsia-800 text-white font-black text-sm">
                     Design Gráfico
                 </h2>
+
+                <div className="reveal opacity-0 flex flex-col items-center w-full 
+                    bg-fuchsia-200 border-2 rounded-md p-2 text-fuchsia-700 mt-2">
+
+                    <h2 className="reveal opacity-0 text-lg font-black tracking-wider">Habilidades Extras</h2>
+
+                    <div className="flex flex-col items-center gap-2 justify-center p-1">
+                        <h3 className="text-sm font-extrabold">Conhecimentos na Area da Saúde</h3>
+
+                        <div className="reveal opacity-0 text-xs grid grid-cols-[repeat(5,max-content)] gap-2">
+                            <p className="reveal opacity-0 rounded-sm border p-1 bg-fuchsia-100">Socorrista</p>
+                            <p className="reveal opacity-0 rounded-sm border p-1 bg-fuchsia-100">Cuidadora</p>
+                            <p className="reveal opacity-0 rounded-sm border p-1 bg-fuchsia-100">Aferir</p>
+                            <p className="reveal opacity-0 rounded-sm border p-1 bg-fuchsia-100">Curativos</p>
+                            <p className="reveal opacity-0 rounded-sm border p-1 bg-fuchsia-100">RCP</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col items-center gap-2 justify-center p-1">
+                        <h3 className="text-sm font-extrabold">Ramo Digital</h3>
+
+                        <div className="text-xs grid grid-cols-[repeat(3,max-content)] gap-2">
+                            <p className="reveal opacity-0 rounded-sm border p-1 bg-fuchsia-100">Social Media</p>
+                            <p className="reveal opacity-0 rounded-sm border p-1 bg-fuchsia-100">Design</p>
+                            <p className="reveal opacity-0 rounded-sm border p-1 bg-fuchsia-100">Edição de Vídeo</p>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
 
             {/* Infos */}
 
-            <h1 id="infos" className="reveal opacity-0 text-xl font-black tracking-wider text-fuchsia-800 border-b-2">
+            <h1 id="infos" className="reveal opacity-0 text-xl font-black tracking-wider text-fuchsia-900 border-b-2">
                 Infos
             </h1>
 
             {/* Card Disponibilidade */}
 
-            <div className="reveal opacity-0 w-11/12 bg-fuchsia-300 text-center flex flex-col justify-center items-center p-2 
+            <div className="reveal opacity-0 w-11/12 bg-fuchsia-200 text-center flex flex-col justify-center items-center p-2 
             rounded-md gap-3 outline-2 outline-fuchsia-700">
 
-                <h1 className="pt-3 font-black tracking-wide text-fuchsia-800">Disponibilidade</h1>
+                <h1 className="pt-3 font-black tracking-wide text-fuchsia-900">Disponibilidade</h1>
 
                 <h2 className="text-fuchsia-900 text-sm font-semibold w-10/12">
                     Estou disponivel para garantir que sua marca seja muito bem representada, não importa o momento
@@ -85,9 +115,9 @@ const AboutMeDetails = () => {
 
             {/* Card Pessoal */}
 
-            <div className="reveal opacity-0 w-11/12 bg-fuchsia-300 text-center flex flex-col justify-center items-center p-2 rounded-md gap-3 border-2 border-fuchsia-700">
+            <div className="reveal opacity-0 w-11/12 bg-fuchsia-200 text-center flex flex-col justify-center items-center p-2 rounded-md gap-3 border-2 border-fuchsia-700">
 
-                <h1 className="text-fuchsia-800 font-black tracking-wider">
+                <h1 className="text-fuchsia-900 font-black tracking-wider">
                     Sobre Mim
                 </h1>
 
@@ -96,7 +126,7 @@ const AboutMeDetails = () => {
                         <h2>Meu nome é Ellen Souza</h2>
                     </li>
                     <li>
-                        <h2>Tenho 1,55m de altura</h2>
+                        <h2>Tenho 1,60m de altura</h2>
                     </li>
                     <li>
                         <h2>Sou de Olinda-PE</h2>
@@ -108,20 +138,20 @@ const AboutMeDetails = () => {
                 <div className="flex flex-col text-sm mb-2 rounded-md w-10/12 
                     text-black tracking-widest font-bold">
 
-                    <table className="border-collapse bg-fuchsia-300 text-fuchsia-900">
+                    <table className="border-collapse bg-fuchsia-100 text-fuchsia-900">
                         <caption className="text-base mb-2 font-black">Manequim</caption>
                         <thead>
                             <tr>
-                                <th className="border-2 border-fuchsia-600 p-1 font-extrabold">Calça</th>
-                                <th className="border-2 border-fuchsia-600 p-1 font-extrabold">Blusa</th>
-                                <th className="border-2 border-fuchsia-600 p-1 font-extrabold">Sapato</th>
+                                <th className="border-2 border-fuchsia-500 p-1 font-extrabold">Calça</th>
+                                <th className="border-2 border-fuchsia-500 p-1 font-extrabold">Blusa</th>
+                                <th className="border-2 border-fuchsia-500 p-1 font-extrabold">Sapato</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td className="border-2 border-fuchsia-600 p-1">36</td>
-                                <td className="border-2 border-fuchsia-600 p-1">P ou PP</td>
-                                <td className="border-2 border-fuchsia-600 p-1">34</td>
+                                <td className="border-2 border-fuchsia-500 p-1">36</td>
+                                <td className="border-2 border-fuchsia-500 p-1">P ou PP</td>
+                                <td className="border-2 border-fuchsia-500 p-1">34</td>
                             </tr>
                         </tbody>
                     </table>
